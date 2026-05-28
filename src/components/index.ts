a2013923bb1f5b33e7d33fce0fd587cb28eeafa5
raguevarra <122047688+raguevarra@users.noreply.github.com>
2026-05-28 16:53:34 -0400

    updated index.ts
    significantly cleaner

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,18 +1,27 @@
-export { DashboardSummary, type DashboardSummaryData } from "./DashboardSummary";
-export { TransactionList, type Transaction } from "./TransactionList";
-export { TransactionForm } from "./TransactionForm";
+// accounts
+export { AccountDropdown } from "./AccountDropdown";
+export { AccountForm } from "./AccountForm";
 export { AccountList, type Account } from "./AccountList";
-export { TransactionCard, type TransactionCardData } from "./TransactionCard"
-export { AccountSelector } from "./AccountSelector"
-export { AccountDropdown } from "./AccountDropdown"
-export { BillCard, type BillCardData } from "./BillCard";
+export { AccountSelector } from "./AccountSelector";
+export { TransferForm } from "./TransferForm";
+
+// bills
 export { AccountBillList } from "./AccountBillList";
-export { default as BillList } from "./BillList"
-export { Modal } from "./Modal"
-export { AddBillModal } from "./AddBillModal"
-export { DeleteBillButton } from "./DeleteBillButton"
-export { EditBillButton } from "./EditBillButton"
-export { AccountForm } from "./AccountForm"
-export { TransferForm } from "./TransferForm"
-export { DashboardTransactionsCard } from "./DashboardTransactionsCard"
-export { DashboardHeader } from "./DashboardHeader"
+export { AddBillModal } from "./AddBillModal";
+export { BillCard, type BillCardData } from "./BillCard";
+export { default as BillList } from "./BillList";
+export { DeleteBillButton } from "./DeleteBillButton";
+export { EditBillButton } from "./EditBillButton";
+
+// dashboard
+export { DashboardHeader } from "./DashboardHeader";
+export { DashboardSummary, type DashboardSummaryData } from "./DashboardSummary";
+export { DashboardTransactionsCard } from "./DashboardTransactionsCard";
+
+// transactions
+export { TransactionCard, type TransactionCardData } from "./TransactionCard";
+export { TransactionForm } from "./TransactionForm";
+export { TransactionList, type Transaction } from "./TransactionList";
+
+// shared
+export { Modal } from "./Modal";
